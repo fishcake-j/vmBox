fac580534b54b355482f108fec8c629bed620cfc
fishcake-j <2783988880@qq.com>
2026-01-31 22:02:07 +0800

    Code
    上传了源代码

---
+++ Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        WebView = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(WebView, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' WebView
+        ' 
+        WebView.AllowExternalDrop = True
+        WebView.CreationProperties = Nothing
+        WebView.DefaultBackgroundColor = Color.White
+        WebView.Dock = DockStyle.Fill
+        WebView.Location = New Point(0, 0)
+        WebView.Name = "WebView"
+        WebView.Size = New Size(1225, 842)
+        WebView.TabIndex = 0
+        WebView.ZoomFactor = 1R
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(14F, 31F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1225, 842)
+        Controls.Add(WebView)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "vmBox"
+        CType(WebView, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents WebView As Microsoft.Web.WebView2.WinForms.WebView2
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.Web.WebView2.Core
+
+Public Class Form1
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 确保 WebView2 初始化完成
+        Await WebView.EnsureCoreWebView2Async()
+        ' 导航到目标网页
+        WebView.CoreWebView2.Navigate("https://copy.sh/v86")
+    End Sub
+    Private Sub NewWindowHandler(sender As Object, e As CoreWebView2NewWindowRequestedEventArgs)
+        e.Handled = True
+        WebView.CoreWebView2.Navigate(e.Uri)
+    End Sub
+
+End Class
